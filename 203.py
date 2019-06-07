4292725bdf9a482096c304806c36826b9502d73f
subayadhav <suba10499@gmail.com>
2019-06-07 14:39:14 +0530

    Update 203.py

--- 203.py
+++ 203.py
@@ -1,12 +1,14 @@
-n1=int(raw_input())
-d={}
-v=["a","e","i","o","u","A","E","I","O","U"]
-for i in range(n1):
-  s1=raw_input()
-  c=0
-  for i in s1:
-    if i in v:
-      c+=1
-  d[s1]=c
-for i in (sorted(d.items(), key = lambda kv:(kv[1], kv[0]),reverse=True)):
-  print(i[0])
+def count(s):
+    k1=0
+    for i in s:
+        if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+            k1=k1+1
+    return k1
+n=int(input())
+l=[]
+for i in range(0,n):
+    s=input()
+    l.append([s,count(s)])
+l.sort(key=lambda x:x[1],reverse=True)
+for i in range(0,len(l)):
+    print(l[i][0])
